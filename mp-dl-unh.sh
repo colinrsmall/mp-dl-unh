f225994684dc3ac01590baca8aa5d783b2e7679a
Colin Small <colinrsmall@yahoo.com>
2019-09-15 19:14:39 -0400

    fixed script again

--- mp-dl-unh.sh
+++ mp-dl-unh.sh
@@ -3,7 +3,7 @@
 echo "$OSTYPE"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then # assume sript is run at the SDC
-    python3 processory.py 2017-01-10T03:10:25 2017-01-10T08:09:00 mms1 > log.log
+    python3 processor.py 2017-01-10T03:10:25 2017-01-10T08:09:00 mms1 > log.log
 
 elif [[ "$OSTYPE" == "darwin19" ]]; then # assume script is run on Colin Small's laptop
     python3 processor.py 2017-01-10T03:10:25 2017-01-10T08:09:00 mms1 > log.log
