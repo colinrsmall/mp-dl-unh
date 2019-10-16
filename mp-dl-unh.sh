d2c5756e49b40b32c0021a96f3b77ce32c1ec689
Colin Small <colinrsmall@yahoo.com>
2019-10-16 13:09:38 -0400

    Changed pipe to tee for the output

--- mp-dl-unh.sh
+++ mp-dl-unh.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$0")"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then # assume sript is run at the SDC
     export PATH=/tools/anaconda3.2018.12/bin
     export PYTHONPATH=/tools/anaconda3.2018.12/lib/python3.6/site-packages
-    python3 processor.py $1 $2 mms1 >> /mms/itfhome/mms-glspro/logs/mp-dl.unh.log
+    python3 processor.py $1 $2 mms1 |& tee /mms/itfhome/mms-glspro/logs/mp-dl.unh.log
 
 elif [[ "$OSTYPE" == "darwin19" ]]; then # assume script is run on Colin Small's laptop
-    python3 processor.py $1 $2 mms1 >> mp-dl-unh_log.log
+    python3 processor.py $1 $2 mms1 |& tee mp-dl-unh_log.log
 fi
 
 #function finally {
